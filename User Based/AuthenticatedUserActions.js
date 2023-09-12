e428c4106d75eabda68c2dba95966e4d2ca239fa
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-09-12 19:55:06 +0530

    Update AuthenticatedUserActions.js

--- User Based/AuthenticatedUserActions.js
+++ User Based/AuthenticatedUserActions.js
@@ -132,10 +132,8 @@ async function DispatchVote(uid, answer, jwt = null) {
 
 async function submitProfileChange(jwt,gender, fname, lname, username, reduceNotifications, hideTopStars, takeBreak, nameInpolls, anonymousMode) {
     const res = await AxiosSigned.post(endpoints["/submitProifleChange"], jwt, {gender, fname, lname, username, reduceNotifications, hideTopStars, takeBreak, nameInpolls, anonymousMode}, null);
-    if (res.data.success === false && res.data.notUnique) {
-        /// user is not unique, print the msg perhaps to the user?
-        var msg = res.data.error;
-        var msgEn = res.data.error_EN; // ignore this
+    if (res.success === false) {
+        return false;
         
         //TODO; DISPLAY!
     }
