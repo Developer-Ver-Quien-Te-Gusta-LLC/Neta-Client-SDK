e0e5423c08b225cd248fa9ba94d81b20b067927e
graham-hologress <developer@descarganeta.com>
2023-10-07 03:50:31 -0700

    Update KV.js

--- utils/KV.js
+++ utils/KV.js
@@ -1,22 +1,56 @@
-import { _post } from "./AxiosSigned.js";
+import { _fetch } from "./KV.js";
 
+var FetchedEndpoints = {
+  "/verifypn/sendotp":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/sendOtp",
+  "/verifypn/verifyotp":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/verifyOTP",
+  "/verifypn/fetchStatus":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/fetchStatus",
+  "/registration/fetchSchools":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/fetchschools",
+  "/submitProfile":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/submitProfile",
+  "/onboarding/addfriends":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/addfriends",
+  "/uploadUserContacts":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/uploadUserContacts",
+  "/setToken":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/setToken",
+  "/generateNewCustomToken":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/generateNewCustomToken",
+  "/uploadpfp":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/uploadpfp",
+  "/checkLocationStatus":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/checkLocationStatus",
+  "/requestDeletion":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/requestDeletion",
+  "/disableDeletion":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/disableDeletion",
+  "/recordEventNaive":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/recordEventNaive",
+  "/checkUsername":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/checkUsername",
+  "/friends/request":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/request",
+  "/friends/accept":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/accept",
+  "/friends/hide":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/hide",
+  "/friends/remove":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/remove",
+  "/friends/block":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/block",
+  "/friends/report":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/report",
+  "/friends/resetBlockList":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/resetBlockList",
+  "/friends/resetHideList":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/friends/resetHideList",
+  "/submitProfileChange":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/submit-profile-change",
+  "/login":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/login",
+  "/refresh":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/Refresh",
+  "/readInbox":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/readInbox",
+  "/forceFetchPolls":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/forceFetchPolls",
+  "/dispatchVote":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/dispatchVote",
+  "/purchase":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/purchase",
+  "/OnPollRevealed":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/revealPoll",
+  "/hideActivity":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/hideActivity",
+  "/invitations/request":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/invitations/request",
+  "/invitations/fetch":"https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/invitations/fetch"
+};
 
-async function _fetch(key) {
-    let params = {}
-    if (!Array.isArray(key)) {
-        params = {"key":key}
-    } else {
-        params = {"keys":key}
-    }
+const FetchEndpointsFromKV = () => {
+  return FetchedEndpoints;
+}
 
-    try{
-        const res = await _post({uri : "https://f6du6bvzz2.execute-api.us-east-1.amazonaws.com/getKV", queryString : params});
-        return res
-    }
-    catch(err){
-        console.log("Error fetching KV");
-        return []
-    }
-} 
+var Callbacks = [];
 
-export  { _fetch };
+async function onEndpointsFetched(Callback){
+  if(FetchedEndpoints != undefined){
+    Callback(true);
+  }
+  else{
+    Callbacks.push(Callback);
+  }
+}
+
+export { onEndpointsFetched, FetchEndpointsFromKV };
+
