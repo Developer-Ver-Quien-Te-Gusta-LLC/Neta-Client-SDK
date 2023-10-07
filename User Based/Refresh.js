2eb6f8ac23519c079f3f7e9e12b95835de1e3c69
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-10-07 04:07:34 -0700

    Update Refresh.js

--- User Based/Refresh.js
+++ User Based/Refresh.js
@@ -16,7 +16,7 @@ var page_SchoolUsers=0
 var page_Contacts = 0;
 
 //TO DO 
-async function RefreshAll(jwt,platform,page_LimitInbox,highschool,grade){
+async function RefreshAll(jwt,platform,page_LimitInbox){
   const requestedScreen = "all";
   const qString = {requestedScreen:requestedScreen,platform:platform,page_LimitInbox:page_LimitInbox,page_FriendsOfFriends:page_FriendsOfFriends,page_SchoolUsers:page_SchoolUsers,page_Contacts:page_Contacts,highschool:highschool,grade:grade};
   page_FriendsOfFriends++;
