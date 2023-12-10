0c555ded7bb65375dc82eab0899e2027dacaf2e0
Daxx <64298070+Daxxboii@users.noreply.github.com>
2023-12-11 03:42:34 +0530

    Friends System Cleanup

--- User Based/FriendSystem.js
+++ User Based/FriendSystem.js
@@ -13,7 +13,6 @@ InitializeEndpoints();
 async function OnFriendRequest(friendUID,jwt) {
   const QueryString = { friend: friendUID };
   const endpoint = endpoints["/friends/request"];
-  //const jwt = Cache.getString("jwt");
 
   const res = await AxiosSigned._post({uri:endpoint,queryString:QueryString,jwt:jwt});
   return res;
@@ -23,64 +22,25 @@ async function AcceptFriendRequest(friendUID,jwt) {
   const endpoint = endpoints["/friends/accept"];
   const QueryString = { friend: friendUID };
 
-  //const jwt = Cache.getString("jwt");
 
   const res = await AxiosSigned._post({uri:endpoint,queryString:QueryString,jwt:jwt});
   return res;
 }
 
-async function HideFriendRequestfriendUID(friendUID,jwt) {
-  const endpoint = endpoints["/friends/hide"];
-  const QueryString = { friend: friendUID };
 
- // const jwt = Cache.getString("jwt");
-
- const res = await AxiosSigned._post({uri:endpoint,queryString:QueryString,jwt:jwt});
-  return res;
-}
 async function RemoveFriend(friendUID,jwt) {
   const endpoint = endpoints["/friends/remove"];
   const QueryString = { friend: friendUID };
 
-  //const jwt = Cache.getString("jwt");
-
-  const res = await AxiosSigned._post({uri:endpoint,queryString:QueryString,jwt:jwt});
-  return res;
-}
-async function BlockFriend(friendUID,jwt) {
-  const endpoint = endpoints["/friends/block"];
-  const QueryString = { friend: friendUID };
-
-  //const jwt = Cache.getString("jwt");
-
   const res = await AxiosSigned._post({uri:endpoint,queryString:QueryString,jwt:jwt});
   return res;
 }
 
-async function ResetBlockList(jwt) {
-  const endpoint = endpoints["/friends/resetBlockList"];
- // const jwt = Cache.getString("jwt");
-
- const res = await AxiosSigned._post({uri:endpoint,queryString:QueryString,jwt:jwt});
-  return res;
-}
-
-async function ResetHideList(jwt) {
-  const endpoint = endpoints["/friends/resetHideList"];
-  //const jwt = Cache.getString("jwt");
-
-  const res = await AxiosSigned._post({uri:endpoint,queryString:QueryString,jwt:jwt});
-  return res;
-}
 
 // #endregion
 
 export {
   OnFriendRequest,
   AcceptFriendRequest,
-  HideFriendRequestfriendUID,
   RemoveFriend,
-  BlockFriend,
-  ResetBlockList,
-  ResetHideList,
 };
