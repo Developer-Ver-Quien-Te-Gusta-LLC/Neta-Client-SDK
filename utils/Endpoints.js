884b521a68c9b9e5ee5930f1097ff56329ccf693
Daxx <64298070+Daxxboii@users.noreply.github.com>
2024-01-17 03:08:37 +0530

    removed local endpoints

--- utils/Endpoints.js
+++ utils/Endpoints.js
@@ -37,49 +37,9 @@ import { _fetch } from "./KV.js";
   "/invitations/fetch": "https://0ioa1u4p5b.execute-api.us-east-1.amazonaws.com/invitations/fetch",
   "/invite":"https://invite-website-jhfmfvn6oa-ue.a.run.app"
 };
-var LocalEndpoints = {
-  "/verifypn/sendotp":"http://localhost:3000/sendOtp",
-  "/verifypn/verifyotp":"http://localhost:3000/verifyOTP",
-  "/verifypn/fetchStatus":"http://localhost:3000/fetchStatus",
-  "/registration/fetchSchools":"http://localhost:3000/fetchschools",
-  "/submitProfile":"http://localhost:3000/submitProfile",
-  "/onboarding/addfriends":"http://localhost:3000/addfriends",
-  "/uploadUserContacts":"http://localhost:3000/uploadUserContacts",
-  "/setToken":"http://localhost:3000/setToken",
-  "/generateNewCustomToken":"http://localhost:3000/generateNewCustomToken",
-  "/uploadpfp":"http://localhost:3000/uploadpfp",
-  "/checkLocationStatus":"http://localhost:3000/checkLocationStatus",
-  "/requestDeletion":"http://localhost:3000/requestDeletion",
-  "/disableDeletion":"http://localhost:3000/disableDeletion",
-  "/recordEventNaive":"http://localhost:3000/recordEventNaive",
-  "/checkUsername":"http://localhost:3000/checkUsername",
-  "/friends/request":"http://localhost:3000/friends/request",
-  "/friends/accept":"http://localhost:3000/friends/accept",
-  "/friends/hide":"http://localhost:3000/friends/hide",
-  "/friends/remove":"http://localhost:3000/friends/remove",
-  "/friends/block":"http://localhost:3000/friends/block",
-  "/friends/report":"http://localhost:3000/friends/report",
-  "/friends/resetBlockList":"http://localhost:3000/friends/resetBlockList",
-  "/friends/resetHideList":"http://localhost:3000/friends/resetHideList",
-  "/submitProfileChange":"http://localhost:3000/submit-profile-change",
-  "/login":"http://localhost:3000/login",
-  "/refresh":"http://localhost:3000/Refresh",
-  "/readInbox":"http://localhost:3000/readInbox",
-  "/forceFetchPolls":"http://localhost:3000/forceFetchPolls",
-  "/dispatchVote":"http://localhost:3000/dispatchVote",
-  "/purchase":"http://localhost:3000/purchase",
-  "/OnPollRevealed":"http://localhost:3000/revealPoll",
-  "/hideActivity":"http://localhost:3000/hideActivity",
-  "/invitations/request":"http://localhost:3000/invitations/request",
-  "/invitations/fetch":"http://localhost:3000/invitations/fetch"
-}
-
-var isLocal = false;
 
 const FetchEndpointsFromKV = () => {
-  if(!isLocal)return FetchedEndpoints;
-  else return LocalEndpoints;
-
+   return LocalEndpoints;
 }
  
 var Callbacks = [];
